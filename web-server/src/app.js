8c9f5570a96d7ebfe8a9383d210fa42fbec43b0c
nadezdatsygankova <nadiapritchina@gamil.com>
2022-10-15 19:38:23 -0400

    view

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -9,23 +9,25 @@ const publicDirectoryPath = (path.join(__dirname, '../public'))
 app.use(express.static(publicDirectoryPath))
 app.set('view engine', 'hbs')
 
-// app.get('/', (req, res) => {
-//   res.send('<h1>Weather</h1>')
-// })
+app.get('/', (req, res) => {
+  res.render('index', {
+    title: 'Weather App',
+    name: 'Nadia Tsygankova'
+    })
+})
 
-// app.get('/help', (req, res) => {
-//   res.send([{
-//     name: 'Andrew',
-//     age: 27
-//   }, {
-//     name: 'Katy',
-//     age: 76
-//   }
-//   ])
-// })
-// app.get('/about', (req, res) => {
-//   res.send('<h1>About</h1>')
-// })
+app.get('/help', (req, res) => {
+  res.render('help', {
+    title: 'Help',
+    name: 'Nadia Tsygankova'
+    })
+})
+app.get('/about', (req, res) => {
+  res.render('about', {
+    title: 'About me',
+    name: 'Nadia Tsygankova'
+    })
+})
 // app.get('/weather', (req, res) => {
 //   res.send({
 //     forecast: 'It is snowing',
