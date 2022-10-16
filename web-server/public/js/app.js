a9a981f38d95a52fb14bf8d447844a82566a9a8f
nadezdatsygankova <nadiapritchina@gamil.com>
2022-10-15 22:42:20 -0400

    fetch browser site

--- web-server/public/js/app.js
+++ web-server/public/js/app.js
@@ -1,1 +1,13 @@
 console.log('Client')
+
+fetch('http://localhost:3000/weather?address=Boston')
+  .then((response) => {
+    response.json().then((data) => {
+      if (data.error) {
+        console.log(data.error)
+      } else {
+        console.log(data.address)
+        console.log(data.temperature)
+      }
+    })
+  })
