a250183ac3138d73081c013b4ffc937df08461f3
nadezdatsygankova <nadiapritchina@gamil.com>
2022-10-15 20:01:41 -0400

    partials

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -1,32 +1,46 @@
 const express = require('express');
 const path = require('path');
+const hbs = require('hbs');//for partials
+
 // console.log(__dirname); //src
 // console.log(__filename); //src/app.js
 const app = express()
+//Define paths for Express config
 const publicDirectoryPath = (path.join(__dirname, '../public'))
+//tell name for directory for all views(templates)
+const viewsPath = path.join(__dirname, '../templates/views')
+//for partial
+const partialsPath = path.join(__dirname, '../templates/partials');
 
-
-app.use(express.static(publicDirectoryPath))
+//Setup handlebars engine and views location
 app.set('view engine', 'hbs')
+//tell express where find a views
+app.set('views', viewsPath)
+//for partial
+hbs.registerPartials(partialsPath);
+
+//Setup static directory to serve
+app.use(express.static(publicDirectoryPath))
 
 app.get('/', (req, res) => {
   res.render('index', {
     title: 'Weather App',
     name: 'Nadia Tsygankova'
-    })
+  })
 })
 
 app.get('/help', (req, res) => {
   res.render('help', {
+    helpText: 'This is a help page',
     title: 'Help',
-    name: 'Nadia Tsygankova'
-    })
+    name: 'Nadia Tsy'
+  })
 })
 app.get('/about', (req, res) => {
   res.render('about', {
     title: 'About me',
     name: 'Nadia Tsygankova'
-    })
+  })
 })
 // app.get('/weather', (req, res) => {
 //   res.send({
