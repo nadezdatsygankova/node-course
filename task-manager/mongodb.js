1d5851eac6ac9237ae41c1a7968b7c9d802b7606
nadezdatsygankova <nadiapritchina@gamil.com>
2022-10-18 22:58:50 -0400

    deleteMany

--- task-manager/mongodb.js
+++ task-manager/mongodb.js
@@ -13,29 +13,47 @@ MongoClient.connect(connectionURL, { useNewUrlParser: true }, (error, client) =>
     return console.log('Unable to connect to database!')
   }
   const db = client.db(databaseName);
-//updateOne
-  db.collection('users').updateOne({
-    _id: new ObjectID("634ba4453eb197d47f33cf06")
-  }, {
-    //increment
-    $inc: {
-      age: 1
-    }
-  }).then((result) => {
-    console.log(result)
-  }).catch((error) => {
-    console.log(error)
-  })
+// //updateOne
+//   db.collection('users').updateOne({
+//     _id: new ObjectID("634ba4453eb197d47f33cf06")
+//   }, {
+//     //increment
+//     $inc: {
+//       age: 1
+//     }
+//   }).then((result) => {
+//     console.log(result)
+//   }).catch((error) => {
+//     console.log(error)
+//   })
 
-  db.collection('tasks').updateMany({
-    completed: false
-  }, {
-    //update for this number
-    $set: {
-      completed: true
-    }
-  }).then((result) => {
-    console.log(result.modifiedCount)
-  }).catch((error) => { console.log(error) })
+  // db.collection('tasks').updateMany({
+  //   completed: false
+  // }, {
+  //   //update for this number
+  //   $set: {
+  //     completed: true
+  //   }
+  // }).then((result) => {
+  //   console.log(result.modifiedCount)
+  // }).catch((error) => { console.log(error) })
+
+
+  db.collection('users').deleteMany({
+    age: 28
+    }).then((result) => {
+    console.log(result)
+    }).catch((error) => {
+    console.log(error)
+    })
+
+  db.collection('tasks').deleteOne({
+    description: "Clean the house"
+    }).then((result) => {
+    console.log(result)
+    }).catch((error) => {
+    console.log(error)
+    })
+
 
 })
