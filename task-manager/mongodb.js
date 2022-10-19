2241a78cbc5783186c0e6e6be4de0b4967b6c7b0
nadezdatsygankova <nadiapritchina@gamil.com>
2022-10-18 22:47:31 -0400

    update

--- task-manager/mongodb.js
+++ task-manager/mongodb.js
@@ -13,25 +13,11 @@ MongoClient.connect(connectionURL, { useNewUrlParser: true }, (error, client) =>
     return console.log('Unable to connect to database!')
   }
   const db = client.db(databaseName);
-  db.collection('users').findOne({ _id: new ObjectID("634ba4453eb197d47f33cf06") }, (error, task) => {
-    if (error) {
-      return console.log('Unable to fetch')
-    }
-    console.log(task)
-  })
-
-  db.collection('tasks').find({ completed: false }).toArray((error, tasks) => {
-    console.log(tasks)
-  })
-
-  db.collection('tasks').find({ completed: false }).count((error, count) => {
-    console.log(count)
-  })
-
-
+//updateOne
   db.collection('users').updateOne({
     _id: new ObjectID("634ba4453eb197d47f33cf06")
   }, {
+    //increment
     $inc: {
       age: 1
     }
@@ -44,16 +30,12 @@ MongoClient.connect(connectionURL, { useNewUrlParser: true }, (error, client) =>
   db.collection('tasks').updateMany({
     completed: false
   }, {
+    //update for this number
     $set: {
       completed: true
     }
   }).then((result) => {
     console.log(result.modifiedCount)
   }).catch((error) => { console.log(error) })
-
-  //   db.collection('tasks').findOne({ _id: new
-  //     ObjectID("5c0fec243ef6bdfbe1d62e2f") }, (error, task) => {
-  //     console.log(task)
-  //     })
 
 })
